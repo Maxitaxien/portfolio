7c9d151430b1b110b39372f9931f1b0888b52d29
Max Bakke Seymour <max.bakke.seymour@gmail.com>
2025-09-16 15:40:41 +0200

    Add app and setup router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { 
+  createBrowserRouter,
+  RouterProvider } from 'react-router-dom';
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import App from './App.tsx'
+import Home from './pages/home';
+import Contact from './pages/contact';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/contact", element: <Contact /> }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
